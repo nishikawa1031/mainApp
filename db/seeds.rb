0b30de9fea32592b49b8278be2fa5051d76d28d2
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-12 20:33:01 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "test@example.com", password: "password", password_confirmation: "password")
+
+Article.create(title: "First Article", body: "This is the first article", status: 0, user_id: 1)
+
+Article.create(title: "Second Article", body: "This is the second article", status: 1, user_id: 1)
+
+Article.create(title: "Third Article", body: "This is the third article", status: 2, user_id: 1)
