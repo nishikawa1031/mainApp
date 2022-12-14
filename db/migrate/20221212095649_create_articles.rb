c1c631173467b96b1a306b4283ab1f9fc72c1823
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-14 22:01:20 +0900

    記事のステータスの初期値をdraftに

--- db/migrate/20221212095649_create_articles.rb
+++ db/migrate/20221212095649_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     create_table :articles do |t|
       t.string :title
       t.text :body
-      t.integer :status
+      t.integer :status, default: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
