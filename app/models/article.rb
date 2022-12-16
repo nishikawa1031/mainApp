c0e697e3722633577b6d2c6f68d3d6c88bf72352
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-16 22:39:12 +0900

    カテゴリのseedデータを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   enum :status, { draft: 0, published: 1, archived: 2, trashed: 3 }
   belongs_to :user
+  has_many :category_articles
+  has_many :categorys, through: :category_articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,27 @@
 
 User.create(email: "test@example.com", password: "password", password_confirmation: "password")
 
+# 下書きの記事
 3.times do |article|   
-  Article.create(title: "Article #{article}", body: "This is article number #{article}", status: 0, user_id: 1)
+  Article.create(title: "Article #{article}", body: "This is article number #{article}", status: 0, user: User.first)
 end
 
+# 公開中の記事
 5.times do |article|   
-    Article.create(title: "Article #{article}", body: "This is article number #{article}", status: 1, user_id: 1)
+    Article.create(title: "Article #{article}", body: "This is article number #{article}", status: 1, user: User.first)
 end
 
+# アーカイブの記事
 Article.create(title: "Third Article", body: "This is the third article", status: 2, user_id: 1)
 
-Category.create(name: "Ruby")
-
-category_articles = CategoryArticle.create(category_id: 1, article_id: 1)
+# カテゴリ
+Category.create(name: '民法')
+Category.create(name: '刑法')
+Category.create(name: '憲法')
+ 
+# 記事、カテゴリのidを列挙して中間テーブルに投入
+Article.all.ids.sort.each do |article_id|
+  Category.all.ids.sort.each do |category_id|
+    CategoryArticle.create(article_id: article_id, category_id: category_id)
+  end
+end
