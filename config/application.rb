6169b12cffee79ade1fbb32a5fad02396db34194
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-18 21:03:59 +0900

    時刻の表示を日本時間に修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module MyProj
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
