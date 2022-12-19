907a3a14ab36a980b35e8cd2ae5741c86bc326a5
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-19 21:34:32 +0900

    サブheaderを動的に生成する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
-    def to_hankaku
-        # Article.subjects.keys.each do |subject|
-        tag.li(class: 'nav-item active', style: "font-size: 14px;") do
-            link_to "民法", articles_path(subject: "civil_law"), class:"nav-link text-light" 
+    def ul_sub_header
+        tag.ul(class: 'navbar-nav mr-auto pl-2') do
+            Article.subjects.keys.each do |subject|
+                li_html(subject)
+            end
         end
     end
+
+    private
+        def li_html(subject)
+            concat(tag.li(class: 'nav-item active', style: "font-size: 14px;") do
+                link_to "#{subject}", articles_path(subject: "#{subject}"), class:"nav-link text-light" 
+            end)
+        end
 end
