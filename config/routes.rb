f79d94e7e1c1f9696f979f286ffceb05a6e41b8e
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-19 23:07:57 +0900

    usernameをparameterにする

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  scope "users/:username" do
+    get "/" => "users#show", as: :user
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
