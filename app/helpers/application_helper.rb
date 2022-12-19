c364236b422b214fd200bb759aa9ef5aa55a8573
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-19 21:15:25 +0900

    [WIP]viewを動的に生成する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def to_hankaku
+        # Article.subjects.keys.each do |subject|
+        tag.li(class: 'nav-item active', style: "font-size: 14px;") do
+            link_to "民法", articles_path(subject: "civil_law"), class:"nav-link text-light" 
+        end
+    end
 end
