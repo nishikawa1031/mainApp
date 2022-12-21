4168637ce8f4ef10b0dad326c55582d982d76049
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-21 21:02:07 +0900

    enumに全教科追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,23 @@
 class Article < ApplicationRecord
   enum :status, { draft: 0, published: 1, archived: 2, trashed: 3 }
-  enum :subject, { others: 0, civil_law: 1, constitution: 2, criminal_law: 3, civil_procedure_law: 4, criminal_procedure_law: 5 }
+  enum :subject, { 
+    civil_law: 0,
+    constitution: 1,
+    criminal_law: 2,
+    civil_procedure_law: 3,
+    criminal_procedure_law: 4,
+    administrative_law: 5,
+    company_law: 6,
+    labor_law: 7,
+    intellectual_property_law: 8,
+    tax_law: 9,
+    bankruptcy_law: 10,
+    economic_law: 11,
+    private_international_law: 12,
+    public_international_law: 13,
+    environmental_law: 14,
+    others: 15
+  }
   belongs_to :user
   has_many :category_articles
   has_many :categories, through: :category_articles
