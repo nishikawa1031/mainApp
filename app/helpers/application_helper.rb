3b870d5e313809bf247bd2f3cbfce4c13fd6c039
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-21 21:17:47 +0900

    選択中の科目にアンダーラインを当てる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,14 @@ module ApplicationHelper
 
     private
         def li_html(subject)
+            undlerline = "
+                border-bottom:solid;
+                border-color:#FFFF00;
+                border-width:5px;
+            " if params[:subject] == subject
+
             concat(tag.li(class: 'nav-item active', style: "font-size: 14px;") do
-                link_to I18n.t("article.subject.#{subject.to_s}"), articles_path(subject: "#{subject}"), class:"nav-link text-light" 
+                link_to I18n.t("article.subject.#{subject.to_s}"), articles_path(subject: "#{subject}"), class:"nav-link text-light" , style: undlerline
             end)
         end
 end
