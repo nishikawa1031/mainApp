6c145eb1626e62d0bebdbeb8f682086da0b368f4
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-27 23:37:24 +0900

    rails g migration AddImpressionsCountToTweets impressions_count:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_143457) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_143606) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_143457) do
     t.boolean "answer", default: false, null: false
     t.integer "year"
     t.text "comment"
+    t.integer "impressions_count", default: 0
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
