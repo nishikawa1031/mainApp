af079544cb56e1b3a4b7e02897dce3d722a55709
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-28 08:05:34 +0900

    bundle exec rails generate migration RemoveViewsFromArticles views:integer

---
+++ db/migrate/20221227230450_remove_views_from_articles.rb
@@ -0,0 +1,5 @@
+class RemoveViewsFromArticles < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :articles, :views, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_143606) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_230450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_143606) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "views", default: 0, null: false
     t.integer "subject", default: 0, null: false
     t.boolean "answer", default: false, null: false
     t.integer "year"
