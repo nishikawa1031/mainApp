9bc1f1612e45ac4b3dfad07222b15034424f5603
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-28 22:26:57 +0900

    usernameのvalidationのテストを作成

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    let(:user) { User.new(username: 'ExampleUser', email: 'user@example.com') }
+
+    describe "バリデーションのテスト" do
+        context "usernameカラム" do
+            it "空欄でないこと" do
+                user.username = ""
+                expect(user).not_to be_valid;
+            end
+
+            it "英数字以外を含まないこと" do
+                user.username = "あいう"
+                expect(user).not_to be_valid
+            end
+        end
+    end
+end
