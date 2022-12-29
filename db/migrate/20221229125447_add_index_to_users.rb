a002b06f8c3412e43c94749ce54570673773223c
nishikawa1031 <tatsunishitatsu@gmail.com>
2022-12-29 22:07:55 +0900

    rubocop -A

--- db/migrate/20221229125447_add_index_to_users.rb
+++ db/migrate/20221229125447_add_index_to_users.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 class AddIndexToUsers < ActiveRecord::Migration[7.0]
-  def change
-  end
+  def change; end
   add_index :users, :username, unique: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new(username: 'ExampleUser', email: 'user@example.com', password: "password") }
+  let(:user) { User.new(username: 'ExampleUser', email: 'user@example.com', password: 'password') }
 
   describe 'バリデーションのテスト' do
     context 'usernameカラム' do
