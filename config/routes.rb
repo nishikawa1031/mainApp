9dfb7c14f22f0a88d6356e6605f7ab8212fbd7a2
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-01-05 22:54:39 +0900

    Google認証 rails g controller GoogleLoginApi callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'google_login_api/callback'
   root to: 'articles#index'
   resources :articles do
     resource :bookmarks, only: %i[create destroy]
@@ -16,6 +17,8 @@ Rails.application.routes.draw do
   scope 'users/:username' do
     get '/' => 'users#show', as: :user
   end
+
+  post '/google_login_api/callback', to: 'google_login_api#callback'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
