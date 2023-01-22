d2b4412165d127536e420a3e57f8df3524915146
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-01-22 14:39:45 +0900

    rails g migration AddfileToArticles file:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_06_142819) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_053733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_142819) do
     t.integer "year"
     t.text "comment"
     t.integer "impressions_count", default: 0
+    t.string "file"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
