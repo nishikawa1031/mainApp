db5abe000e3fd521c72569e0f725eb77a64efcbd
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-04-05 07:04:41 +0900

    問題分の表示に成功

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,10 @@
 class QuestionsController < ApplicationController
+    def show
+        year = params[:year_id].to_i
+        subject = params[:subject_id]
+        question_num = params[:id].to_i
+        yml = File.open("config/files/all/minpo2015.ja.yml") { |f| YAML.load(f) }
+
+        @questions = yml["ja"]["minpo"][year][question_num]
+    end
 end
