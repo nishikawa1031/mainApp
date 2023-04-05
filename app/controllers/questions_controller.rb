69bcf67d918b80df25f852d60ab50565e8958571
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-04-05 21:12:20 +0900

    全科目表示できるよう修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,14 @@
 class QuestionsController < ApplicationController
     def show
         year = params[:year_id].to_i
-        subject = params[:subject_id]
+        subject = params[:subject_id].to_i
         question_num = params[:id].to_i
-        yml = File.open("config/files/all/minpo2015.ja.yml") { |f| YAML.load(f) }
+        subject_name = "minpo" if subject == 1
+        subject_name = "kenpo" if subject == 2
+        subject_name = "keiho" if subject == 3
+        yml = File.open("config/files/all/#{subject_name}#{year}.ja.yml") { |f| YAML.load(f) }
 
-        question_set = yml["ja"]["minpo"][year][question_num]
+        question_set = yml["ja"][subject_name][year][question_num]
         
         @question = question_set["questions"]
         @answer = question_set["answer"].values.filter_map { |v| v if v }
