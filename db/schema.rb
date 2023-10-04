5eae3f1cf4db201249d35417dcaea14db94483d8
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-04 18:45:58 +0900

    bundle exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_093256) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_093941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,6 +123,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_093256) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "person_articles", force: :cascade do |t|
+    t.bigint "person_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_person_articles_on_article_id"
+    t.index ["person_id"], name: "index_person_articles_on_person_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -148,4 +157,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_093256) do
   add_foreign_key "category_articles", "categories"
   add_foreign_key "likes", "articles"
   add_foreign_key "likes", "users"
+  add_foreign_key "person_articles", "articles"
+  add_foreign_key "person_articles", "people"
 end
