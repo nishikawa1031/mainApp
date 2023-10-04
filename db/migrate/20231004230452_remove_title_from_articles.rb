aa2f1bc3de7e16e4dd6ac436c91e12749e84ba92
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-05 08:05:35 +0900

    rails generate migration RemoveTitleFromArticles title:string comment:text

---
+++ db/migrate/20231004230452_remove_title_from_articles.rb
@@ -0,0 +1,6 @@
+class RemoveTitleFromArticles < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :articles, :title, :string
+    remove_column :articles, :comment, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_112427) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_230452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,12 +43,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_112427) do
   end
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
     t.text "body"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "comment"
     t.integer "impressions_count", default: 0
     t.string "file"
     t.datetime "start_time"
