a5bb2e45a7628711cd8ef7a20d968cbc10384e88
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-04 20:25:19 +0900

    rails generate migration RemoveStatusFromArticles status:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_093941) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_112427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_093941) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "status", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
