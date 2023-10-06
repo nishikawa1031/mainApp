d2ae1f0d601565719bc74a2adb1f739b18b8cdbc
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-06 21:55:28 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,61 +8,75 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# user1
+# 織田信長の日記
 user1 = User.create(email: 'test@example.com', username: 'taroyama', password: 'password',
                     password_confirmation: 'password')
 
-# user2
-user2 = User.create(email: 'another_test@example.com', username: 'another_man', password: 'password',
-                    password_confirmation: 'password2')
-
-# user3
-User.create(email: 'another_test2@example.com', username: 'another_man2', password: 'password',
-            password_confirmation: 'password2')
-
-Article.create(
-  body: 'a' * 1000,
-  user: User.first,
-  start_time: Time.zone.now + 3.day,
-  end_time: Time.zone.now + 3.day,
-)
-
-historical_people = [
-  { name: "織田信長", user_id: 1 },
-  { name: "明智光秀", user_id: 2 },
-  { name: "豊臣秀吉", user_id: 3 },
-  { name: "徳川家康" },
-  { name: "武田信玄" },
-  { name: "上杉謙信" },
-  { name: "今川義元" },
-  { name: "北条氏康" }
-]
-
-historical_people.each do |person_data|
-  Person.create(person_data)
-end
-
-# Historical People's Diaries
-historical_diaries = {
-  "織田信長" => "本能寺の変が近づいている気がする。しかし、我が野望は終わることはない。",
-  "明智光秀" => "信長様との間に亀裂が生じ始めている。私の行動が日本の歴史を変えるだろうか？",
-  "豊臣秀吉" => "天下統一への道は険しくも、私の策略と魅力でこれを成し遂げる。",
-  "徳川家康" => "関ヶ原の戦いが成功すれば、新しい時代が始まる。平和な日本を築くための準備を始めるべきだ。",
-  "武田信玄" => "風林火山。私の旗印のもと、私は今日も戦場を駆け巡る。",
-  "上杉謙信" => "信玄との合戦は名将同士の戦いとなるだろう。しかし、我が信念を貫くことは譲れない。",
-  "今川義元" => "桶狭間への進軍を計画中。これによって歴史がどう変わるか、楽しみである。",
-  "北条氏康" => "関東地方を我が物とするため、日々策略を巡らせている。"
-}
-
-# 織田信長の日記エントリの作成
+## 明智光秀との面会に関する日記
 nobunaga_meeting_mitsuhide_diary = Article.create(
   body: "今日は明智光秀と重要な面会を持った。彼の策略や智慧には常に感銘を受けている。しかし、彼の瞳には未解決の謎が隠れているようにも見える。我々の関係がこれからどのように展開していくのか、興味深く思う。",
-  user_id: 1, # 織田信長のuser_id
+  user_id: user1.id, # 織田信長のuser_id
   start_time: Time.zone.now - 2.days,
   end_time: Time.zone.now - 2.days + 2.hours
 )
 
-# 明智光秀との関連付け
-mitsuhide = Person.find_by(name: '明智光秀')
+mitsuhide = Person.create(name: "明智光秀", user_id: 2)
 PersonArticle.create(article_id: nobunaga_meeting_mitsuhide_diary.id, person_id: mitsuhide.id)
 
+## 豊臣秀吉との面会に関する日記
+nobunaga_meeting_hideyoshi_diary = Article.create(
+  body: "今日は豊臣秀吉との重要な面会があった。彼の野心と実力は明らかで、我が信頼する部下として彼を見る。彼との共同作業はこの国の未来を形成するだろう。",
+  user_id: user1.id, # 織田信長のuser_id
+  start_time: Time.zone.now - 4.days,
+  end_time: Time.zone.now - 4.days + 2.hours
+)
+
+hideyoshi = Person.create(name: "豊臣秀吉", user_id: 3)
+PersonArticle.create(article_id: nobunaga_meeting_hideyoshi_diary.id, person_id: hideyoshi.id)
+
+## 武田信玄との面会に関する日記
+nobunaga_meeting_shingen_diary = Article.create(
+  body: "武田信玄との対面はいつも緊張感が走る。彼の武勇と知略は我が敵として尊敬する。しかし、彼との共闘の可能性も考えられなくはない。",
+  user_id: user1.id, # 織田信長のuser_id
+  start_time: Time.zone.now - 7.days,
+  end_time: Time.zone.now - 7.days + 2.hours
+)
+
+shingen = Person.create(name: "武田信玄")
+PersonArticle.create(article_id: nobunaga_meeting_shingen_diary.id, person_id: shingen.id)
+
+# 豊臣秀吉の日記
+user2 = User.create(email: 'another_test@example.com', username: 'another_man', password: 'password', password_confirmation: 'password2')
+
+# 織田信長との面会に関する日記エントリの作成
+hideyoshi_meeting_nobunaga_diary = Article.create(
+  body: "今日、織田信長様との重要な会議があった。彼の大志と指導力は天下統一への道を明確に示している。彼のもとで働くことは、私にとって大きな誇りである。",
+  user_id: user2.id,
+  start_time: Time.zone.now - 5.days,
+  end_time: Time.zone.now - 5.days + 2.hours
+)
+
+nobunaga = Person.create(name: '織田信長')
+PersonArticle.create(article_id: hideyoshi_meeting_nobunaga_diary.id, person_id: nobunaga.id)
+
+# 明智光秀との面会に関する日記エントリの作成
+hideyoshi_meeting_mitsuhide_diary = Article.create(
+  body: "明智光秀との対話はいつも洞察に富んでいる。彼の戦術や思考は、戦場での成功に不可欠だ。しかし、彼には何か隠していることがあるようにも感じられる。",
+  user_id: user2.id,
+  start_time: Time.zone.now - 3.days,
+  end_time: Time.zone.now - 3.days + 2.hours
+)
+
+mitsuhide = Person.find_by(name: '明智光秀')
+PersonArticle.create(article_id: hideyoshi_meeting_mitsuhide_diary.id, person_id: mitsuhide.id)
+
+# 武田信玄との面会に関する日記エントリの作成
+hideyoshi_meeting_shingen_diary = Article.create(
+  body: "武田信玄との交渉は難しさを伴うものだった。彼の軍事的才能と戦略は非常に尊敬している。我々の間に協力の可能性があるかどうかは未だ不明だが、彼との関係を維持することは重要だ。",
+  user_id: user2.id,
+  start_time: Time.zone.now - 10.days,
+  end_time: Time.zone.now - 10.days + 2.hours
+)
+
+shingen = Person.find_by(name: '武田信玄')
+PersonArticle.create(article_id: hideyoshi_meeting_shingen_diary.id, person_id: shingen.id)
