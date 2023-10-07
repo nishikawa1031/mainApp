8dda4982eccd32967061e7a46576ace9a20d5782
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-07 22:21:48 +0900

    rails db:migrate:reset

--- db/migrate/20231006222038_add_creater_id_columns_people.rb
+++ db/migrate/20231006222038_add_creater_id_columns_people.rb
@@ -2,6 +2,6 @@
 
 class AddCreaterIdColumnsPeople < ActiveRecord::Migration[7.0]
   def change
-    add_column :person, :creater_id, :integer, null: false
+    add_column :people, :creater_id, :integer, null: false
   end
 end
