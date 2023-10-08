002a601e0677d6ab7724c72a07d8cb3980027e70
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-08 12:09:14 +0900

    callbackのuriを本番と開発環境で分ける

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def current_user?(user)
     user == current_user
   end
+
+  def google_callback_uri
+    Rails.env.production? ? 'https://www.ldocs.jp/google_login_api/callback' : 'http://localhost:3000/google_login_api/callback'
+  end
 end
