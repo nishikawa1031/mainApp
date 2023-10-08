6585b6096f1c99ed51fb88b8ecd35bc0da1aa625
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-08 15:26:41 +0900

    routesからdeviseに関する記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,6 @@ Rails.application.routes.draw do
     resource :bookmarks, only: %i[create destroy]
     resource :likes, only: %i[create destroy]
   end
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
 
   post '/google_login_api/callback', to: 'google_login_api#callback'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
