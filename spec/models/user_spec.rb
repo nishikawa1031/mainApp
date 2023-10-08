971be95cc45d302f667bfeb25c1ab0ba42c4fce7
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-08 15:15:24 +0900

    不要なテストの削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,26 +20,5 @@ RSpec.describe User, type: :model do
   let(:user) { User.new(email: 'user@example.com', password: 'password') }
 
   describe 'バリデーションのテスト' do
-    context 'usernameカラム' do
-      it '空欄でないこと' do
-        user.username = ''
-        expect(user).not_to be_valid
-      end
-
-      it '英数字以外を含まないこと' do
-        user.username = 'あいう'
-        expect(user).not_to be_valid
-      end
-
-      it '英数字以外を含まないこと' do
-        user.username = 'dddd234'
-        expect(user).to be_valid
-      end
-
-      it 'アンダースコアを登録できること' do
-        user.username = 'dd_dd234'
-        expect(user).to be_valid
-      end
-    end
   end
 end
