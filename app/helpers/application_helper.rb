501ac019f5c2221bf49e1cacbdfe0c8ca097713c
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-10-09 18:28:41 +0900

    不要なメソッドの削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,13 +6,4 @@ module ApplicationHelper
   def current_user?(user)
     user == current_user
   end
-
-  def google_callback_uri
-    Rails.env.production? ? 'https://www.ldocs.jp/google_login_api/callback' : 'http://localhost:3000/google_login_api/callback'
-  end
-
-  def authenticate_user!
-    redirect_to home_index_path unless current_user
-    # ユーザーがログインしていない限り、ログイン画面にレダイレクトする
-  end
 end
