bb484852435731ea648bc429c0442d8842c4b015
nishikawa1031 <tatsunishitatsu@gmail.com>
2023-11-02 21:37:33 +0900

    remove omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -10,17 +10,9 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   get 'home/index'
 
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'log_out', to: 'sessions#destroy', as: 'log_out'
-
-  resources :sessions, only: %i[create destroy]
-  get 'google_login_api/callback'
   root to: 'articles#index'
   resources :articles do
     resource :bookmarks, only: %i[create destroy]
     resource :likes, only: %i[create destroy]
   end
-  # https://github.com/omniauth/omniauth#integrating-omniauth-into-your-application
-  post '/auth/:provider/callback', to: 'sessions#create'
 end
