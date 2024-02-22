5ef286133892dd18d4398f9bcc6cf66a37f75d9c
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-02-23 00:39:44 +0900

    デプロイを成功させるために一時的にコメントアウト

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  skip_before_action :check_logged_in, only: :index
+  # skip_before_action :check_logged_in, only: :index
 
   def index; end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  skip_before_action :check_logged_in, only: :create
+  # skip_before_action :check_logged_in, only: :create
 
   def create
     if (user = User.find_or_create_from_auth_hash(auth_hash))
