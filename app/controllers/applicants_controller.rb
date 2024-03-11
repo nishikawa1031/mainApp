174e823b29d3810869516cd1aefbe76bae3c242f
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-03-12 07:20:09 +0900

    rails g scaffold_controller applicants

---
+++ app/controllers/applicants_controller.rb
@@ -0,0 +1,58 @@
+class ApplicantsController < ApplicationController
+  before_action :set_applicant, only: %i[ show edit update destroy ]
+
+  # GET /applicants
+  def index
+    @applicants = Applicant.all
+  end
+
+  # GET /applicants/1
+  def show
+  end
+
+  # GET /applicants/new
+  def new
+    @applicant = Applicant.new
+  end
+
+  # GET /applicants/1/edit
+  def edit
+  end
+
+  # POST /applicants
+  def create
+    @applicant = Applicant.new(applicant_params)
+
+    if @applicant.save
+      redirect_to @applicant, notice: "Applicant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /applicants/1
+  def update
+    if @applicant.update(applicant_params)
+      redirect_to @applicant, notice: "Applicant was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /applicants/1
+  def destroy
+    @applicant.destroy
+    redirect_to applicants_url, notice: "Applicant was successfully destroyed.", status: :see_other
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_applicant
+      @applicant = Applicant.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def applicant_params
+      params.fetch(:applicant, {})
+    end
+end
