a73be2b0eefb6bc10acece71c8027eccac39ab38
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-03-13 08:29:48 +0900

    rails db:migrate

--- db/migrate/20240312230930_applicant.rb
+++ db/migrate/20240312230930_applicant.rb
@@ -1,4 +1,10 @@
-class Applicant < ActiveRecord::Migration[7.0]
+class AddColumnsToApplicants < ActiveRecord::Migration[7.0]
   def change
+    add_column :applicants, :date_of_birth, :date
+    add_column :applicants, :age, :integer
+    add_column :applicants, :nationality, :integer
+    add_column :applicants, :phone_number, :integer
+    add_column :applicants, :self_introduction, :text
+    add_column :applicants, :desired_salary, :text
   end
 end
