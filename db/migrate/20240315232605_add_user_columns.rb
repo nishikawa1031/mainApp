6697b5269d6d6d212646aae49baa25ec985c19f8
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-03-16 08:26:55 +0900

    rails generate migration AddUserColumns username:string

---
+++ db/migrate/20240315232605_add_user_columns.rb
@@ -0,0 +1,5 @@
+class AddUserColumns < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_12_230930) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_15_232605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -172,6 +172,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_12_230930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
