3de9765bc3c9297f6a3f1f23dc8f05129639ffe3
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-04-18 23:18:49 +0900

    ActiveStorageはファイル用のカラムは不要

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_18_140639) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_18_141757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,7 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_140639) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "impressions_count", default: 0
-    t.string "file"
     t.datetime "start_time"
     t.datetime "end_time"
     t.string "title"
