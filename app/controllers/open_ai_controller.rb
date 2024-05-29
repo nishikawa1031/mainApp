e2df4e80d77c046ef4cd6e777e69763753c981d9
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-05-29 21:23:44 +0900

    CSVファイルのパスを修正しました。

--- app/controllers/open_ai_controller.rb
+++ app/controllers/open_ai_controller.rb
@@ -6,7 +6,8 @@ class OpenAiController < ApplicationController
     client = OpenAI::Client.new
 
     # image = params[:image]
-    image_path = "./app/assets/images/illustrator/404068.png"
+    file_name = '404068'
+    image_path = "./app/assets/images/illustrator/#{file_name}.png"
     image_data = Base64.encode64(File.read(image_path))
     image_url = "data:image/jpeg;base64,#{image_data}"
 
@@ -14,6 +15,12 @@ class OpenAiController < ApplicationController
     The key of the response stores each letter of the sign and the value is the number of balls.The response must be returned in json format.
     example response:
     { 'R': 18, 'e': 14, 'f': 12, 'l': 9, 'e': 14, 'c': 10, 't': 10 }
+
+    If there is a mark that is not a letter, key should be 'Not a letter'.
+    ex. { 'Not a letter': 148, 'M': 88, 'a': 54, 'r': 28, 'u': 38, 'z': 44, 'e': 54, 'n': 39 }
+
+    If there are identical letters, the second and subsequent letters must be followed by a number at the end of the key name.
+    ex. {'H'=>47, 'A'=>49, 'M'=>57,'A2'=>49, 'M2'=>57,'A3'=>49, 'T'=>46, 'S'=>43, 'U'=>43}
     "
 
     messages = [
@@ -36,9 +43,7 @@ class OpenAiController < ApplicationController
     puts response_json = JSON.parse(@response_text)
 
     # Define the CSV file path
-    csv_file_path = Rails.root.join('public', 'output.csv')
-
-    file_name = '404068'
+    csv_file_path = Rails.root.join('./app/assets/images/csv/after', "#{file_name}.csv")
 
     generate_csv(csv_file_path, response_json, file_name)
 
