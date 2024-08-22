1e619da7e3e083cf24f263e73555791827c29d5a
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-08-23 07:34:43 +0900

    feat: Add creater_id column to applicants table

--- db/migrate/20240309203528_change_people_to_applicant.rb
+++
@@ -1,5 +0,0 @@
-class ChangeapplicantToApplicant < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :applicant, :applicants
-  end
-end
--- db/migrate/20240309203745_change_person_articles_to_applicant_articles.rb
+++
@@ -1,5 +0,0 @@
-class ChangePersonArticlesToApplicantArticles < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :person_articles, :applicant_articles
-  end
-end
