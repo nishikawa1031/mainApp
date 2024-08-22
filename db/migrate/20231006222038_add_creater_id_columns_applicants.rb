f60a5bbfbe19b1464a2d783e3ff52dad4de187f6
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-08-23 07:42:49 +0900

    Remove migration file for adding creater_id column to applicants table

--- db/migrate/20231006222038_add_creater_id_columns_applicants.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddCreaterIdColumnsApplicants < ActiveRecord::Migration[7.0]
-  def change
-    add_column :applicants, :creater_id, :integer, null: false
-  end
-end
