50bf962b23eff40c96f329f0ce76e711d9b65ac7
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-08-23 07:45:19 +0900

    chore: Update migration file to change null constraint for name column in applicants table

--- db/migrate/20231007035953_change_name_null_constraint_in_applicants.rb
+++ db/migrate/20231007035953_change_name_null_constraint_in_applicants.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class ChangeNameNullConstraintInApplicant < ActiveRecord::Migration[7.0]
+class ChangeNameNullConstraintInApplicants < ActiveRecord::Migration[7.0]
   def change
-    change_column :applicant, :name, :string, null: false
+    change_column :applicants, :name, :string, null: false
   end
 end
