fe47ed4ff9b390d97c5628f8c8d043683c15a995
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-09-04 22:34:17 +0900

    feat: Add internship page and helper method

--- app/helpers/applicants_helper.rb
+++ app/helpers/applicants_helper.rb
@@ -3,11 +3,16 @@
 module ApplicantsHelper
   def privacy_policy
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    markdown.render(File.read(Rails.root.join('privacy_policy.md')))
+    markdown.render(File.read(Rails.root.join('markdown/privacy_policy.md')))
   end
 
   def service_term
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    markdown.render(File.read(Rails.root.join('service_term.md')))
+    markdown.render(File.read(Rails.root.join('markdown/service_term.md')))
+  end
+
+  def internship
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown.render(File.read(Rails.root.join('markdown/internship.md')))
   end
 end
