3daf93d15d4561be64634a26ce590583c9e2f047
nishikawa1031 <tatsunishitatsu@gmail.com>
2024-09-08 06:26:44 +0900

    chore: Update CORS origins in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://www.solution-hub.net', 'https://main-app-1209-44817d8898b5.herokuapp.com/'
+    origins 'http://localhost:3000', 'https://www.solution-hub.net', 'https://main-app-1209-44817d8898b5.herokuapp.com/', 'https://job-in-japan.com/'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
